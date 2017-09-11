a18e7c49cce6f614d4dff50cf57068726c1c02c4
Kayhan <kdehghan@sfu.ca>
2017-09-11 12:40:30 -0700

    Add java implementation of insertion sort and iterative and recursive quick sort

--- Algorithms/SearchAndSort/Java Implementation/SearchAndSortAlgorithms.java
+++ Algorithms/SearchAndSort/Java Implementation/SearchAndSortAlgorithms.java
@@ -1,6 +1,5 @@
-package Algorithms;
-
 import java.util.Arrays;
+import java.util.Stack;
 
 public class SearchAndSortAlgorithms {
 
@@ -43,19 +42,19 @@ public class SearchAndSortAlgorithms {
 	public static void mergeSort(int[] array) {
 		int middle;
 		int size;
-		if( array.length > 1) {
+		if (array.length > 1) {
 			size = array.length;
-			middle =  size/2;
-			int[] leftArray = Arrays.copyOfRange(array, 0, middle );
+			middle = size / 2;
+			int[] leftArray = Arrays.copyOfRange(array, 0, middle);
 			int[] rightArray = Arrays.copyOfRange(array, middle, size);
-			
+
 			mergeSort(leftArray);
 			mergeSort(rightArray);
-			
+
 			merge(array, leftArray, rightArray);
 		}
 	}
-	
+
 	/*
 	 * ============================================================
 	 * Pre: <array1> and <array2> are sorted in ascending order
@@ -64,23 +63,120 @@ public class SearchAndSortAlgorithms {
 	 * ============================================================
 	 */
 	private static void merge(int[] orginal, int[] leftArray, int[] rightArray) {
-		int idx = 0, indx1 = 0, indx2 = 0; 
-		
-		while( indx1 < leftArray.length && indx2 < rightArray.length ) {
-			if( leftArray[indx1] < rightArray[indx2]) {
-				orginal[idx++] = leftArray[indx1++];	
-			}
-			else {
-				orginal[idx++] = rightArray[indx2++];	
+		int idx = 0, indx1 = 0, indx2 = 0;
+
+		while (indx1 < leftArray.length && indx2 < rightArray.length) {
+			if (leftArray[indx1] < rightArray[indx2]) {
+				orginal[idx++] = leftArray[indx1++];
+			} else {
+				orginal[idx++] = rightArray[indx2++];
 			}
 		}
-		
-		while( indx1 < leftArray.length ) {
+
+		while (indx1 < leftArray.length) {
 			orginal[idx++] = leftArray[indx1++];
 		}
-		while( indx2 < rightArray.length ) {
-			orginal[idx++] = rightArray[indx2++];	
+		while (indx2 < rightArray.length) {
+			orginal[idx++] = rightArray[indx2++];
 		}
 	}
-	
+
+	/*
+	 * ============================================================
+	 * Iterative in-place Insertion Sort for arrays on integers in
+	 * ascending order
+	 * ============================================================
+	 */
+	public static void insertionSort(int[] input) {
+		for (int i = 1; i < input.length; i++) {
+			int currentElement = input[i];
+			int j = i - 1;
+			while (j >= 0 && currentElement < input[j]) { // the order of logical AND is important
+				input[j + 1] = input[j--];
+			}
+			input[j + 1] = currentElement;
+		}
+	}
+
+	/*
+	 * ============================================================
+	 * Recursive in-place Quick Sort for arrays on integers in
+	 * scending order	 
+	 * ============================================================
+	 */
+	public static void quickSortRecursive(int[] input) {
+		quickSortRecursiveHelper(input, 0, input.length - 1);
+	}
+
+	private static void quickSortRecursiveHelper(int[] input, int low, int high) {
+		if (high > low) { // implicit base case
+			int splitIndx = partition(input, low, high);
+			quickSortRecursiveHelper(input, low, splitIndx - 1);
+			quickSortRecursiveHelper(input, splitIndx + 1, high);
+		}
+	}
+
+	/*
+	 * ============================================================
+	 * Iterative Quick Sort for arrays on integers in
+	 * scending order	 
+	 * ============================================================
+	 */
+	public static void quickSortIterative(int[] input) {
+		quickSortIterativeHelper(input, 0, input.length - 1);
+	}
+
+	private static void quickSortIterativeHelper(int[] input, int low, int high) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(low);
+		stack.push(high);
+
+		while(!stack.empty()) {
+			high = stack.pop();
+			low = stack.pop();
+			int splitIndx = partition(input, low, high);
+
+			if(splitIndx - 1 > low) { // if there is a left side, partition it next
+				stack.push(low);
+				stack.push(splitIndx - 1);
+			}
+			
+			if(splitIndx + 1 < high) { // if there is a right side, partition it next
+				stack.push(splitIndx + 1);
+				stack.push(high);
+			}
+		}
+	}
+
+	/*
+	 * ============================================================
+	 * Pivot around the element at 'high' index so that all 
+	 * elements smaller than it are to its left and all elements 
+	 * larger to its right.
+	 * returns the split index.
+	 * ============================================================
+	 */
+	private static int partition(int[] array, int low, int high) {
+		int pivot = array[high];
+		int splitIndx = low - 1;
+		for (int i = low; i < high; i++) {
+			if (array[i] < pivot) {
+				splitIndx++;
+				swap(array, i, splitIndx); // move smaller element to left side of pivot index
+			}
+		}
+		swap(array, ++splitIndx, high);
+		return splitIndx;
+	}
+
+	/*
+	 * ============================================================
+	 * Swap the elements at indexes i and j
+	 * ============================================================
+	 */
+	private static void swap(int[] array, int i, int j) {
+		int temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
 }
--- Algorithms/SearchAndSort/Java Implementation/Test.java
+++ Algorithms/SearchAndSort/Java Implementation/Test.java
@@ -1,6 +1,5 @@
-package Algorithms;
-
 public class Test {
+	private static int[] srtArray1, srtArray2, srtArray3;
 
 	public static void main(String[] args) {
 		
@@ -16,24 +15,80 @@ public class Test {
 		}
 		
 		// Test Merge Sort
-		int[] testArray = {94, -3, 34, 23, 9, 10};
+		int[] testArray1 = {94, -3, 34, 23, 9, 10};
+		int[] testArray2 = {94};
+		int[] testArray3 = {94, 92, 9, -9, -88};
 		
-		SearchAndSortAlgorithms.mergeSort(testArray);
+		srtArray1 = testArray1.clone();
+		srtArray2 = testArray2.clone();
+		srtArray3 = testArray3.clone();
 		
-		boolean failed = false;
-		for (int i=0; i< testArray.length-1; i++) {
-			if( testArray[i] > testArray[i+1] ) {
-				failed = true;
-			} 
-		}
+		SearchAndSortAlgorithms.mergeSort(srtArray1);
+		SearchAndSortAlgorithms.mergeSort(srtArray2);
+		SearchAndSortAlgorithms.mergeSort(srtArray3);
 		
-		if(failed) {
+		if(isSortedAscending(srtArray1) && isSortedAscending(srtArray2) && isSortedAscending(srtArray3)) {
+			System.out.println("Success: merge sort is working.");
+		} else {
 			System.out.println("Error: merge sort is NOT working!");
 		}
-		else {
-			System.out.println("Success: merge sort is working.");
+		
+		srtArray1 = testArray1.clone();
+		srtArray2 = testArray2.clone();
+		srtArray3 = testArray3.clone();
+		SearchAndSortAlgorithms.insertionSort(srtArray1);
+		SearchAndSortAlgorithms.insertionSort(srtArray2);
+		SearchAndSortAlgorithms.insertionSort(srtArray3);
+		
+		if(isSortedAscending(srtArray1) && isSortedAscending(srtArray2) && isSortedAscending(srtArray3)) {
+			System.out.println("Success: insertion sort is working.");
+		}else {
+			System.out.println("Error: insertion sort is NOT working!");
 		}
 		
+		srtArray1 = testArray1.clone();
+		srtArray2 = testArray2.clone();
+		srtArray3 = testArray3.clone();
+		SearchAndSortAlgorithms.quickSortRecursive(srtArray1);
+		SearchAndSortAlgorithms.quickSortRecursive(srtArray2);
+		SearchAndSortAlgorithms.quickSortRecursive(srtArray3);
+
+		if(isSortedAscending(srtArray1) && isSortedAscending(srtArray2) && isSortedAscending(srtArray3)) {
+			System.out.println("Success: quick sort (recursive) is working.");
+		}else {
+			System.out.println("Error: quick sort (recursive) is NOT working!");
+		}
+
+		srtArray1 = testArray1.clone();
+		srtArray2 = testArray2.clone();
+		srtArray3 = testArray3.clone();
+		SearchAndSortAlgorithms.quickSortIterative(srtArray1);
+		SearchAndSortAlgorithms.quickSortIterative(srtArray2);
+		SearchAndSortAlgorithms.quickSortIterative(srtArray3);
+
+		if(isSortedAscending(srtArray1) && isSortedAscending(srtArray2) && isSortedAscending(srtArray3)) {
+			System.out.println("Success: quick sort (iterative) is working.");
+		}else {
+			System.out.println("Error: quick sort (iterative) is NOT working!");
+		}
+		
+	}
+
+	private static boolean isSortedAscending(int[] array) {
+		for (int i=0; i< array.length-1; i++) {
+			if( array[i] > array[i+1] ) {
+				return false;
+			} 
+		}
+		return true;
+	}
+
+	private static void printArray(int[] array) {
+		System.out.println("------------------------------------------------");
+		for(int el : array) {
+			System.out.print(el + "\t");
+		}
+		System.out.println("\n------------------------------------------------");
 	}
 
 }
